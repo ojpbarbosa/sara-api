d2e5c1ecd6eee1c8c18770dc314f68d8ed144d02
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-08-28 17:13:04 -0300

    feat(infrastructure): add flask adapters

---
+++ source/infrastructure/flask/adapters/flask_route_adapter.py
@@ -0,0 +1,11 @@
+from flask import Request, Response
+
+from source.application.ports.presentation.controller import Controller
+
+
+class FlaskRouteAdapter:
+    def __init__(self, controller: Controller):
+        self.controller = controller
+
+    def route(self, request: Request, response: Response):
+        return self.controller.handle(request)
