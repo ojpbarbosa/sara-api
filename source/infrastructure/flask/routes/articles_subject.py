c4674bd89750445ef4acfa47c462056a72cc192b
Gabriel Willian Bartmanovicz <obielwb@gmail.com>
2022-08-29 11:48:15 -0300

    chore(flask): add some imports

--- source/infrastructure/flask/routes/articles_subject.py
+++ source/infrastructure/flask/routes/articles_subject.py
@@ -1,5 +1,8 @@
 from abc import abstractmethod
-from flask_restful import Resource, Api # install flask-restful
+from flask_restful import Resource, Api 
+
+from source.main.factories.controllers.article.find_article_by_subject_controller_factory import FindArticleBySubjectControllerFactory
+
 
 class ArticlesSubject(Resource): 
     @abstractmethod
